6f1bc4f11f43e0277a694bc7975a8705f8651578
matthias-knopp@gmx.net <matthias-knopp@gmx.net>
2017-11-19 12:13:59 +0100

    Added some new dependencies for the api

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
 var AschJS = require('asch-js');
 var request = require('request');
+var cryptolib = require('./lib/crypto.js');
 
 
+
+//test auccount
 let secret = 'intact rough funny foam say wish river rain minimum stand distance arrest'; //
 
 let publicKey = AschJS.crypto.getKeys(secret).publicKey;
-
 
 var postData = { "publicKey": publicKey };
 var url = 'http://45.32.248.33:4096/api/accounts/open2/';
@@ -17,9 +19,7 @@ var options = {
   url: url 
 }
 
+//this could be also called via the api
 request(options, function(err, res, body) {
-
-    console.log(err);
+    console.log(body);
 });
-
-// curl -X POST -H "Content-Type: application/json" -k -d '{"publicKey":"bd1e78c5a10fbf1eca36b28bbb8ea85f320967659cbf1f7ff1603d0a368867b9"}'    
--- helpers/account.js
+++ helpers/account.js
@@ -1,16 +1,16 @@
-// var crypto = require('../lib/crypto.js');
+var crypto = require('../lib/crypto.js');
 
-// module.exports = {
-// 	account: function (secret) {
-// 		var kp = crypto.keypair(secret);
-// 		var address = crypto.getAddress(new Buffer(kp.publicKey, 'hex'));
+module.exports = {
+	account: function (secret) {
+		var kp = crypto.keypair(secret);
+		var address = crypto.getAddress(new Buffer(kp.publicKey, 'hex'));
 
-// 		return {
-// 			keypair: kp,
-// 			address: address,
-// 			secret : secret
-// 		}
-// 	},
+		return {
+			keypair: kp,
+			address: address,
+			secret : secret
+		}
+	},
 	
-// 	isValidSecret: crypto.isValidSecret
-// }
+	isValidSecret: crypto.isValidSecret
+}
