fcf7f5d767c82e24dddc2915eaabb18e2a9c83b2
matthias-knopp@gmx.net <matthias-knopp@gmx.net>
2017-11-19 18:58:35 +0100

    Added .gitignore file

--- renderer.js
+++ renderer.js
@@ -1,4 +1,30 @@
 
+Vue.component('app-navigation', {
+  data: function() {
+    return {
+      links:[
+        {
+          title: 'Home'
+        },
+        {
+          title: 'About'
+        }
+      ]
+    }
+  },
+  template: `
+      <nav class="navbar navbar-default">
+        <div class="navbar-header">
+          <ul class="nav navbar-nav">
+            <li> <a>Home</a> </li>
+            <li> <a>About</a> </li>
+          </ul> 
+        </div>
+      </nav>
+    `
+});
+
+
 new Vue({
   el: '#app',
   data: {
